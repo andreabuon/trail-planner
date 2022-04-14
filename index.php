cb8354db1454246f9f05f960f0e397d657738b2e
Andrea Buonaurio <buonaurio.1894266@studenti.uniroma1.it>
2022-04-14 17:03:42 +0200

    Separato Index e Navbar. Sistemato struttura cartelle

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="eng">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TrailPlanner</title>
+    <link href="css/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <?php 
+      #require_once 
+    ?>
+  </head>
+  <body>
+    <?php
+      include 'navbar.html';
+    ?>
+    <script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
