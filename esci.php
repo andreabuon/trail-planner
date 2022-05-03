3bbf11539013e61e76a68143aff573aab879b3ed
Andrea Buonaurio <buonaurio.1894266@studenti.uniroma1.it>
2022-05-03 21:26:14 +0200

    sistemato logout

--- esci.php
+++ esci.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
-    echo "Logout Successfully ";
-    echo "<a href='index.php'>Clicca qui</a>";
+    echo "Disconnessione eseguita. ";
+    echo "<a href='index.php'>Clicca qui per tornare alla homepage</a>";
     unset($_SESSION['username']);
     session_destroy();
 ?>
