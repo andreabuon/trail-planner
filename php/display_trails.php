92b8d739dce14c17cf98f88b03440b87acd584af
Andrea Buonaurio <buonaurio.1894266@studenti.uniroma1.it>
2022-05-13 17:56:46 +0200

    - Query restituisce array JSON di oggetti sentieri - da implementare funzione Javascript che crea card per ogni sentiero

--- php/display_trails.php
+++ php/display_trails.php
@@ -1,25 +1,11 @@
 <?php
-    function new_Card($sentiero){
-        echo "
-            <div class='card'>
-                <div class='card-body'>
-                    <h5 class='card-title'> $sentiero[sigla] - $sentiero[nome]</h5>
-                    <p class='card-text'> Lunghezza: $sentiero[lunghezza] Km; Dislivello: $sentiero[dislivello]m </p>
-                    <button class='btn btn-outline-info' onclick='scaricaSentiero();'>Scarica</button>
-                    <button class='btn btn-outline-info' onclick='disegnaSentiero(sentiero);'>Visualizza</button>
-                </div>
-            </div>";
-    }
-    
     include 'database.php';
-    $query = 'SELECT parco_nome, sigla, nome, dislivello, lunghezza, difficolta  FROM sentieri';
+    $query = 'select json_agg(sentieri.*) as lista from sentieri';
     $result = pg_query($query) or die('Query Failed: '.pg_last_error());
     
-    while($line = pg_fetch_array($result, null, PGSQL_ASSOC)){
-        new_Card($line);
-    }
-
-    pg_free_result($result);
-    pg_close($dbconn);
+    $line = pg_fetch_result($result, 0, 'lista');
+    echo $line;
     
+    pg_free_result($result);
+    pg_close($dbconn);  
 ?>
