08ba22772fa235fe5c08d7b98f56539c56493459
Andrea Buonaurio <buonaurio.1894266@studenti.uniroma1.it>
2022-05-16 22:58:37 +0200

    - Configurazione Database da file esterno al server (lento :C)

--- api/database.php
+++ api/database.php
@@ -1,16 +1,18 @@
 <?php
     class Database{
         private static $conn_string;
-        //sistemare!!!!
-        //caricare da file di configurazione
         public static function init(){
-            self::$conn_string = 'host=localhost port=5432 dbname=progettoLTW_db user=andrea password=password'; 
+            if(!isset(self::$conn_string)){
+                //self::$conn_string = 'host=localhost port=5432 dbname=progettoLTW_db user=andrea password=password'; 
+                $config = parse_ini_file($_SERVER['DOCUMENT_ROOT'].'/../config.ini');
+                //brutto ma con le doppie virgolette non funge
+                self::$conn_string = 'host=' . $config['host'] . ' port=' . $config['port'] . ' dbname=' . $config['dbname'] . ' user=' . $config['user'] . ' password=' . $config['password']; ;
+            }
         }
 
         public static function connect(){
-            //Database::init();
+            Database::init();
             return pg_connect(self::$conn_string);
         }
     }
-    Database::init();
 ?>
