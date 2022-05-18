1e69acdfbe2e112d0571bb3212b3391615540581
Andrea Buonaurio <buonaurio.1894266@studenti.uniroma1.it>
2022-05-18 17:42:57 +0200

    - Parametrizzato Link Navbar

--- navbar.php
+++ navbar.php
@@ -1,56 +1,49 @@
 <link rel='stylesheet' href='css/navbar.css'>
 
+<?php
+    class navItem{
+        private $text;
+        private $href;
+
+        function __construct($text, $href){
+            $this->text = $text;
+            $this->href = $href;
+        }
+
+        function __toString(){
+            if(basename($_SERVER['SCRIPT_NAME'])==$this->href) 
+                return '<li class="nav-item"><a class="nav-link active" href="'.$this->href.'">'.$this->text.'</a></li>';
+            else
+                return '<li class="nav-item"><a class="nav-link" href="'.$this->href.'">'.$this->text.'</a></li>';
+        }
+    }
+    function stampaNavs($array){
+        foreach($array as $el)
+        echo (new navItem($el[0], $el[1]));
+    }
+?>
+
 <nav class='navbar navbar-expand-md navbar-dark' id='navbarAndrea'>
         <a class='navbar-brand' href='index.php'>TrailPlanner</a>
-        <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+        <button class='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav'>
             <span class='navbar-toggler-icon'></span>
         </button>
+        
         <div class='collapse navbar-collapse' id='navbarNav'>
             <ul class='navbar-nav'>
-                <li class='nav-item'>
-                    <a class='nav-link' href='esplora.php'>Esplora</a>
-                </li>
-                <li class='nav-item'>
-                    <a class='nav-link' href='partecipa.php'>Partecipa</a>
-                </li>
+                <?php
+                    $l_items = array(['Esplora', 'esplora.php'], ['Partecipa', 'partecipa.php']);
+                    stampaNavs($l_items);
+                ?>
             </ul>
             <ul class='navbar-nav ms-auto'>
-                
                 <?php
                     session_start();
                     if(isset($_SESSION['username'])){
-                        echo '
-                        <!--
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="escursioni.php">Escursioni</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="tracce.php">Tracce</a>
-                        </li>
-                        -->
-                        <li class="nav-item">
-                            <a class="nav-link" href="carica.php">Carica</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="organizza.php">Organizza</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" >'.$_SESSION['username'].'</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="esci.php">Esci</a>
-                        </li>
-                        ';
-                    }else{
-                        echo '
-                        <li class="nav-item">
-                            <a class="nav-link" href="registrati.php">Registrati</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="accedi.php">Accedi</a>
-                        </li>
-                        ';
-                    }
+                        $r_items = array(['Carica', 'carica.php'],['Organizza', 'organizza.php'], [$_SESSION['username'], ''], ['Esci', 'esci.php']);
+                    }else
+                        $r_items = array(['Registrati', 'registrati.php'], ['Accedi', 'accedi.php']);
+                    stampaNavs($r_items);
                 ?>
             </ul>
        </div>
