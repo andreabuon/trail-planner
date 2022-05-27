7ab6d97bdf8fa57c0f7ffcce67a12393185b80e0
Andrea Buonaurio <buonaurio.1894266@studenti.uniroma1.it>
2022-05-27 12:41:38 +0200

    Homepage v2 sfondo non opaco + card bianca con due bottoni fixata in fondo

--- index.php
+++ index.php
@@ -14,20 +14,15 @@
 		include 'alerts.php';
 	?>
 
-	<div id='content container'>
-		<div class="row">
-			<img src="assets/img/1.jpg" class="w-100">
-		</div>
-		<div class="row">
-			<h5 class='text-center'>TrailPlanner</h5>
-			<p class='text-center'>Organizza le tue escursioni con TrailPlanner.</p>
-		</div>
-		<div class='row d-flex'>
-			<a class='btn btn-success col' href='esplora.php'>Esplora</a>
-			<a class='btn btn-success col' href='partecipa.php'>Partecipa</a>
+	<div id='content' class='container-fluid'>
+		<div class='card text-center'>
+			<h1 class='card-title'>TrailPlanner</h1>
+			<h4 class='card-subtitle'>Organizza le tue escursioni con noi</h4>
+			<div class='btn-wrapper'>
+				<a class='btn btn-lg btn-outline-success' href='esplora.php'>Esplora Percorsi</a>
+				<a class='btn btn-lg btn-outline-success' href='partecipa.php'>Prenota Escursioni</a>
+			</div>
 		</div>
 	</div>
 </body>
-<script src='js/popper.min.js'></script>
-
 </html>
